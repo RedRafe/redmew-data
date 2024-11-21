d8bb7ea48c6bfc52be87e98531a81116afb6b8e1
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-11-21 10:07:58 +0100

    Update DO:pyanodon

--- danger-ores/pyanodon/data-2/buffs.lua
+++ danger-ores/pyanodon/data-2/buffs.lua
@@ -5,3 +5,20 @@ for _, armor in pairs(data.raw.armor) do
     armor.inventory_size_bonus = 20 + armor.inventory_size_bonus
   end
 end
+
+if not data.raw['transport-belt']['turbo-transport-belt'] then
+  data.raw['transport-belt']['express-transport-belt'].speed = 0.125
+  data.raw['underground-belt']['express-underground-belt'].speed = 0.125
+  data.raw['splitter']['express-splitter'].speed = 0.125
+  data.raw['loader']['express-loader'].speed = 0.125
+  
+  if data.raw['loader-1x1']['aai-express-loader'] then
+    data.raw['loader-1x1']['aai-express-loader'].speed = 0.125
+  end
+  if data.raw['loader-1x1']['express-transport-belt-loader'] then
+    data.raw['loader-1x1']['express-transport-belt-loader'].speed = 0.125
+  end
+  if data.raw['furnace']['express-transport-belt-beltbox'] then
+    data.raw['furnace']['express-transport-belt-beltbox'].crafting_speed = 4
+  end
+end
