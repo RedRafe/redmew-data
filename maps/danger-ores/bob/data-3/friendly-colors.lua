a4728afd36c761218ebf2257d1d59ee95e814ec5
RedRafe <gabri97.gt+redrafe@gmail.com>
2026-01-13 10:39:08 +0100

    Update DO:BA colors

--- maps/danger-ores/bob/data-3/friendly-colors.lua
+++ maps/danger-ores/bob/data-3/friendly-colors.lua
@@ -1,11 +1,11 @@
 local PALETTE = redmew.palette
 
--- Advanced belts
-redmew.entity_friendly_color('transport-belt',   'bob-turbo-transport-belt',   PALETTE['green'])
-redmew.entity_friendly_color('underground-belt', 'bob-turbo-underground-belt', PALETTE['green'])
-redmew.entity_friendly_color('splitter',         'bob-turbo-splitter',         PALETTE['green'])
+-- Turbo belts
+redmew.entity_friendly_color('transport-belt',   'bob-turbo-transport-belt',   PALETTE['lavender'])
+redmew.entity_friendly_color('underground-belt', 'bob-turbo-underground-belt', PALETTE['lavender'])
+redmew.entity_friendly_color('splitter',         'bob-turbo-splitter',         PALETTE['lavender'])
 
--- Superior belts
-redmew.entity_friendly_color('transport-belt',   'bob-ultimate-transport-belt',   PALETTE['purple'])
-redmew.entity_friendly_color('underground-belt', 'bob-ultimate-underground-belt', PALETTE['purple'])
-redmew.entity_friendly_color('splitter',         'bob-ultimate-splitter',         PALETTE['purple'])
+-- Ultimate belts
+redmew.entity_friendly_color('transport-belt',   'bob-ultimate-transport-belt',   PALETTE['peas'])
+redmew.entity_friendly_color('underground-belt', 'bob-ultimate-underground-belt', PALETTE['peas'])
+redmew.entity_friendly_color('splitter',         'bob-ultimate-splitter',         PALETTE['peas'])
